16c4c42eede0c25de4ad4e1bf2a0f6fb05c66253
Raynanda Aqiyas <raynanda.aqiyas.ft21@mail.umy.ac.id>
2022-12-28 17:48:03 +0700

    fix inorder function

--- Binary Search Tree/Program.cs
+++ Binary Search Tree/Program.cs
@@ -77,7 +77,13 @@ namespace Binary_Search_Tree
             if(ROOT == null)
             {
                 Console.WriteLine("Tree is empty");
-
+                return;
+            }
+            if (ptr != null)
+            {
+                inorder(ptr.leftchild);
+                Console.Write(ptr.info + " ");
+                inorder(ptr.rightchild);
             }
         }
         public void preorder(Node ptr)
