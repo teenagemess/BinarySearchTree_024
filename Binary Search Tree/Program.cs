76ddae5cbf6b9d68cec7582f8876ddf0ab7e9886
Raynanda Aqiyas <raynanda.aqiyas.ft21@mail.umy.ac.id>
2022-12-28 17:11:58 +0700

    add Node class

--- Binary Search Tree/Program.cs
+++ Binary Search Tree/Program.cs
@@ -6,8 +6,30 @@ using System.Threading.Tasks;
 
 namespace Binary_Search_Tree
 {
+    class Node
+    {
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+
+        //Constructor for the NOde Class
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
+    }
+    // a Node class consists of three things, the information, references to the right child, and references
+
+
     internal class Program
     {
+        public Node ROOT;
+        public Program()
+        {
+            ROOT = null;
+        }
         static void Main(string[] args)
         {
         }
